b9ef5d152510af120c2b12074149335863dc94f2
Egus7 <eggualanc@utn.edu.ec>
2023-07-18 00:46:41 -0500

    APIRest facturacion - MongoDB

--- cnn.js
+++ cnn.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const { MongoClient } = require('mongodb');
 const { ObjectId } = require('mongodb');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
